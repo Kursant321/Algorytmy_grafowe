ae5081481c1ecb3b24897663b3cb89dd696d4ab7
Mateusz Kursa <mkursa@student.agh.edu.pl>
2025-12-17 15:14:32 +0100

    Checking if chordal done!

---
+++ lab4/zadanie1.py
@@ -0,0 +1,131 @@
+from dimacs import *
+import os
+'''Poniższy algorytm będzie sprawdzał czy graf jest grafem przekatniowym
+(grafem typu chordal)'''
+
+'''Reprezentacja grafu'''
+class Node:
+    def __init__(self, idx):
+        self.idx = idx
+        self.out = set()
+    
+    def connect_to(self, v):
+        self.out.add(v)
+    
+'''bydowanie grafu z listy krawędzi L i liczby wierzchołków V'''
+
+def bulid_graph(V, L):
+
+    G = [None] + [Node(i) for i in range(1, V + 1)]
+
+    for (u, v, _) in L:
+        G[u].connect_to(v)
+        G[v].connect_to(u)
+    
+    return G
+
+def lexbfs(G):
+    
+    n = len(G) - 1
+    visited = [False] * (n+1)
+
+    order = []
+    blocks = []
+    rest = [i for i in range(2, n + 1)]
+    if rest:
+        blocks.append(rest)
+    blocks.append([1]) # zakładamy, że starujemy algorytm w wierzchołku o indeksie 1
+
+    while blocks:
+        
+        last_block = blocks[-1]
+        v = last_block.pop()
+
+        if visited[v]:
+            continue
+
+        visited[v] = True
+        order.append(v)
+        
+        v_neighbours = G[v].out
+        new_blocks = []
+
+        for X in blocks:
+            Y = []
+            K = []
+            for x in X:
+                if x in v_neighbours:
+                    Y.append(x)
+                else:
+                    K.append(x)
+            if K:
+                new_blocks.append(K)
+            if Y:
+                new_blocks.append(Y)
+        blocks = new_blocks
+    return order
+
+'''Teraz napiszę program sprawdzający czy kolejność
+którą zwraca LexBfs jest PEO. Żeby sprawdzić więc, czy zadany porządek jest kolejnością idealnej eliminacji, 
+wystarczy dla każdego wierzchołka v policzyć RN(v) oraz parent(v), 
+i sprawdzić czy zbiór RN(v) - {parent(v)} jest zawarty w RN(parent(v)).'''
+
+def is_peo(G, order):
+    n = len(G) - 1
+    pos = [0] *(n + 1)
+
+    # indeks v w order
+    for i, v in enumerate(order):
+        pos[v] = i
+    
+    for v in order:
+        # RN(v) - sąsiedzi którszy są wcześniej w order
+        RNv = set()
+        for u in G[v].out:
+            if pos[u] < pos[v]:
+                RNv.add(u)
+        
+        if not RNv:
+            continue
+        # parent(v) - najpóźniejszy element RN(v) (max po pos[])
+        parent = max(RNv, key=lambda u: pos[u])
+
+        # RN(parent(v)) analogicznie
+        RNp = set()
+        for u in G[parent].out:
+            if pos[u] < pos[parent]:
+                RNp.add(u)
+        
+        if not (RNv - {parent} <= RNp):
+            return False
+    
+    return True
+
+def chordal_check(V, L):
+    G = bulid_graph(V, L)
+    order = lexbfs(G)
+    return is_peo(G, order)
+
+
+def check_folder(folder="chordal"):
+    for fname in os.listdir(folder):
+        path = os.path.join(folder, fname)
+
+        if not os.path.isfile(path):
+            continue
+        if fname.endswith(".py"):
+            continue
+
+        V, L = loadWeightedGraph(path)
+
+        expected = int(readSolution(path))      
+        got = int(chordal_check(V, L))          
+
+        if got != expected:
+            print("FAIL:", path, " expected=", expected, " got=", got)
+
+    print("All test passed")
+
+if __name__ == "__main__":
+    check_folder("chordal")
+
